03500dbdf86ac1e45aa144c2e598c549badf4368
BogomolSergo <breinkiller650@gmail.com>
2017-03-30 20:46:15 +0300

    task5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :images, :as => :imageable
+  has_many :images, :as => :imageable, dependent: :destroy
   attr_accessible :title, :body
   validates :id, :presence => true
+
+  scope :published_posts, -> { where('published_at IS NOT NULL') }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :username, :password, :email, :birthday
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :images, :as => :imageable
 
-  validates :username, :password, :uniqueness => { :case_sensitive => false }
+  validates :username, :email, :uniqueness => true
   validates :password, :length => { :minimum => 8 }
   validates :username, :email, :password, :presence => true
+
+  scope :older_than_18, -> { where('birthday < ?', 18.year.ago) }
 
   def full_name
     self.first_name + ' ' + self.last_name
